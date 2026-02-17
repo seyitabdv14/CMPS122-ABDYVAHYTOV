e1c66617b6a9de25515f3f3d6344f6a62dd285b9
seyitabdv14 <sabdyvahytov@stu.bau.edu>
2026-02-17 18:35:27 -0500

    Add Java File Project CMPS122

---
+++ ABDYVAHYTOV_SEYITMUHAMMT_JavaProject/src/Main.java
@@ -0,0 +1,7 @@
+// SEYITMUHAMET ABDYVAHYTOV
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, my name is SEYITMUHAMMET ABDYVAHYTOV.");
+        System.out.println("Today is my first Java project in VS Code.");
+    }
+}
